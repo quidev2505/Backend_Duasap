21a6a9d83f89d11740b28d0d62dd4864baf41b51
--replace-all <quidev2505@gmail.com>
2023-08-14 11:33:03 +0700

    ok fix

--- index.js
+++ index.js
@@ -35,12 +35,6 @@ app.use(express.urlencoded({ extended: true }))
 //API
 app.use('/api/order', routerOrder);
 
-//Static file
-app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
-})
 
 const PORT = 5000 || process.env.PORT
 
